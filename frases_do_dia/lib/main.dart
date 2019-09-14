45cfcfc82934b77439e087cd4c77cd02329fe359
José Mosqueiro <Furos@Furos-A.home>
2019-09-14 22:58:56 +0100

    Img upload

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -6,11 +6,13 @@ void main() {
     debugShowCheckedModeBanner: false,
     title: "Frases do dia",
     home: Container(
-      //color: Colors.white,
       margin: EdgeInsets.only(top: 40),
-      decoration:
-          BoxDecoration(border: Border.all(width: 3, color: Colors.white)),
-      child: Image.asset("resources/mesa.jpg"),
+      decoration: BoxDecoration(
+            border: Border.all(width: 3, color: Colors.white)
+      ),
+      child: Image.asset(
+        "resources/mesa.jpg"
+        ),
     ),
   ));
 }
