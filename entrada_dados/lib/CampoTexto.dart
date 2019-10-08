05642a828a4543b92ea3aaa5aec298f9f5dee67e
jmosqueiro <furosktim@icloud.com>
2019-10-08 18:34:47 +0200

    Possibilidades de entrada de texto e guardar os dados com botao ou onChange

--- entrada_dados/lib/CampoTexto.dart
+++ entrada_dados/lib/CampoTexto.dart
@@ -6,6 +6,9 @@ class CampoTexto extends StatefulWidget {
 }
 
 class _CampoTextoState extends State<CampoTexto> {
+
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +26,29 @@ class _CampoTextoState extends State<CampoTexto> {
                 labelText: "Valor"
               ),
               //enabled: false,
-              maxLength: 3,
+              //maxLength: 3,
+              //maxLengthEnforced: false,
+              //obscureText: true,
+              
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.green
+              ),
+              /* onChanged: (String texto){
+                  print("valor dig" + texto);
+              }, */
+              onSubmitted: (String texto){
+                print("valor dig" + texto);
+              },
+              controller: _textEditingController,
             ),
+          ),
+          RaisedButton( //capturar texto com um botao - uso do controller e onSubmitted
+            child: Text("Save"),
+            color: Colors.lightGreen,
+            onPressed: (){
+              print("valor dig" + _textEditingController.text );
+            },
           )
         ],
       )
